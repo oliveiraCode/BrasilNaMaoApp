9bab94993b2f4f2df01b62414c857e6cb5001af1
Leandro Oliveira <leandro.oliveira@live.com>
2018-12-26 13:16:21 -0500

    Basic project

---
+++ BrasilNaMao/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BrasilNaMao
+//
+//  Created by Leandro Oliveira on 2018-12-26.
+//  Copyright © 2018 OliveiraCode Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
